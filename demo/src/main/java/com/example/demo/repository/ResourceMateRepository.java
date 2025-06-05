adc25d2647683eea72d9383a406c00631a550356
Nandini Sharma <nandinisharma0037@gmail.com>
2025-06-05 10:53:16 +0530

    Moved demo contents to root folder

--- demo/src/main/java/com/example/demo/repository/ResourceMateRepository.java
+++
@@ -1,8 +0,0 @@
-package com.example.demo.repository;
-
-import com.example.demo.entity.ResourceMate;
-import org.springframework.data.mongodb.repository.MongoRepository;
-
-public interface ResourceMateRepository extends MongoRepository<ResourceMate, String> {
-    ResourceMate findByEmail(String email);
-}
